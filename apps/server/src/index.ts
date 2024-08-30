12648946dc464f21d7e5b5cadfe39b1decd0bded
seiya1998 <fukushi1998@gmail.com>
2024-08-30 20:29:01 +0900

    build: :wrench: fastifyの設定を追加

---
+++ apps/server/src/index.ts
@@ -0,0 +1,18 @@
+import fastify from 'fastify';
+
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+const server = fastify();
+
+server.get('/ping', async (request, reply) => {
+    return 'pong\n'
+})
+
+server.listen(PORT, HOST, (err, address) => {
+    if (err) {
+        console.error(err)
+        process.exit(1)
+    }
+    console.log(`Server listening at ${address}`)
+})
