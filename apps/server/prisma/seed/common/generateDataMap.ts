6f4b589b6a0532ef130abed6ea33733d333f20ec
seiya1998 <fukushi1998@gmail.com>
2024-09-13 02:09:53 +0900

    refactor: :unicorn_face: 純粋関数の作成

--- apps/server/prisma/seed/common/generateDataMap.ts
+++ apps/server/prisma/seed/common/generateDataMap.ts
@@ -3,6 +3,18 @@ import { Prisma } from '@prisma/client'
 // Mapを生成する関数を定義
 // この関数は、Promiseを受け取り、Mapを返す
 // K は Map のキーの型
+
+const generateMap = <T, K>(
+    data: T[],
+    keySelector: (item: T) => K
+): Map<K, string> => {
+    const map = new Map<K, string>();
+    data.forEach((item: any) => {
+        map.set(keySelector(item), item.id);
+    });
+    return map;
+}
+
 export const generateMapFromDB = async <T, K>(
     getDataFromDB: (prisma: Prisma.TransactionClient) => Promise<T[]>,
     keySelector: (item: T) => K,
@@ -10,9 +22,5 @@ export const generateMapFromDB = async <T, K>(
 ): Promise<Map<K, string>> => {
     const data = await getDataFromDB(prisma);
 
-    const map = new Map<K, string>();
-    data.forEach((item: any) => {
-        map.set(keySelector(item), item.id);
-    });
-    return map;
+    return generateMap(data, keySelector);
 }
